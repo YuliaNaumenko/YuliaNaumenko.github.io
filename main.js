6b8c30f277b0fcb561958b1b8fef0b11e2b76071
Yulia Naumenko <yna@gecko.no>
2017-04-02 18:03:37 +0300

    Replaced callbacks with Promises and used strings interpolation in templates

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 class Stream {
     constructor(data) {
         if (data === void 0) { data = {}; }
@@ -19,101 +20,82 @@ class Stream {
 }
 
 class QueryInfo {
-    constructor(searchString, totalCount, currentPage) {
-        this.SearchString = searchString || "";
-        this.TotalCount = totalCount || 0;
-        this.CurrentPage = currentPage || 1;
+    constructor(searchString, totalCount = 0, currentPage = 1, itemsPerPage = 5) {
+        this.SearchString = searchString;
+        this.TotalCount = totalCount;
+        this.CurrentPage = currentPage;
+        this.ItemsPerPage = itemsPerPage;
 
-        Object.defineProperty(this, 'TotalPages', {
+        Object.defineProperty(this, "TotalPages", {
             enumerable: true,
             get: () => { return this.TotalCount > 0 ? Math.ceil(this.TotalCount / 5) : 0; }
         });
     }
 }
 
- 
 const CallbackRegistry = {};
 
 class JSONPHandler {
-    constructor(url, onSuccess, onError){
-        let scriptOk = false;
-        const callbackName = 'cb' + String(Math.random()).slice(-6);
-        url += ~url.indexOf('?') ? '&' : '?';
-        // client id should be read from configuration
-        url += 'client_id=luqvyoa6utwz5evwsxo0tkjd3bdjk3&callback=CallbackRegistry.' + callbackName;
 
-        CallbackRegistry[callbackName] = function(data) {
-            scriptOk = true;
-            document.body.removeChild(document.getElementById(callbackName));
-            delete CallbackRegistry[callbackName];
-            onSuccess(data);
-        };
+    fetchTwitchData(url, clientId = "luqvyoa6utwz5evwsxo0tkjd3bdjk3") {
+        return new Promise((resolve, reject) => {
+            let scriptOk = false;
+            const callbackName = `cb${String(Math.random()).slice(-6)}`;
+            url = `${url}${~url.indexOf("?") ? "&" : "?"}client_id=${clientId}&callback=CallbackRegistry.${callbackName}`;
 
-        function checkCallback() {
-            if (scriptOk) return;
-            delete CallbackRegistry[callbackName];
-            onError(url);
-        }
+            CallbackRegistry[callbackName] = function(data) {
+                scriptOk = true;
+                document.body.removeChild(document.getElementById(callbackName));
+                delete CallbackRegistry[callbackName];
+                resolve(data);
+            };
 
-        const script = document.createElement('script');
-        script.id = callbackName;
+            const script = document.createElement("script");
+            script.id = callbackName;
+            script.onload = script.onerror = () => {
+                if (scriptOk) return;
+                delete CallbackRegistry[callbackName];
+                reject(url);
+            };
+            script.src = url;
 
-        script.onreadystatechange = function() {
-            if (this.readyState == 'complete' || this.readyState == 'loaded') {
-                this.onreadystatechange = null;
-                setTimeout(checkCallback, 0);
-            }
-        }
-
-        script.onload = script.onerror = checkCallback;
-        script.src = url;
-
-        document.body.appendChild(script);
+            document.body.appendChild(script);
+        });
     }
 }
 
 class SearchStreamService {
     constructor() {
-        const _this = this;
-        this.parseResult = function (result) {
-            const totalCount = result._total;
-            const streams = result.streams || [];
-            const parsedStreams = [];
-            for (let i = 0; i < streams.length; i++) { 
-                parsedStreams.push(new Stream(streams[i]));
-            }
-            _this.fnCallback(parsedStreams, totalCount);
-        }
+        this.jpHandler = new JSONPHandler();
     }
 
-    getStreamCollection(searchString, fnCallback, pageNumber, limit) {
-        let url = "https://api.twitch.tv/kraken/search/streams?query=";
-        url += encodeURIComponent(searchString);
+    getStreamCollection(searchString, pageNumber, limit) {
+        let url = `https://api.twitch.tv/kraken/search/streams?query=${encodeURIComponent(searchString)}`;
         if (typeof (limit) !== "number" || limit <= 0) {
             limit = 5;
         }
         if (typeof (pageNumber) === "number" && pageNumber > 1) {
             const offset = (pageNumber - 1) * limit;
-            url += '&offset=' + offset;
+            url = `${url}&offset=${offset}`;
         }
-        url += '&limit=' + limit;
-        this.fnCallback = fnCallback;
-        new JSONPHandler(url, this.parseResult, this.errorHandler);
-    }
+        url = `${url}&limit=${limit}`;
 
-    errorHandler(result) {
-        console.error(result);
-    };
+        return this.jpHandler.fetchTwitchData(url).then(result => {
+            const totalCount = result._total;
+            const streams = result.streams || [];
+            const streamItems = [];
+            streams.forEach(stream => { 
+                streamItems.push(new Stream(stream));
+            });
+            return { streamItems, totalCount };
+        });
+    }
 }
 
 class DOMFunctions {
     getElementByClassName(name) {
         const elements = document.getElementsByClassName(name);
-        if (elements.length > 0) {
-            return elements[0];
-        } else {
-            return null;
-        }
+        return elements.length > 0 ? elements[0] : null;
     }
 
     bindEventByClassName(eventName, className, handlerFunction) {
@@ -121,13 +103,6 @@ class DOMFunctions {
         if (element !== null) {
             element.addEventListener(eventName, handlerFunction);
         }
-    }
-
-    getTemplateWithData (data, template){
-        for (let prop in data) {
-            template = template.replace("{{" + prop + "}}", data[prop])
-        }
-        return template;
     }
 }
 
@@ -147,98 +122,108 @@ class Spinner {
 }
 
 class StreamsList {
-    constructor(viewHolderSelector) {
-        const _this = this;
-        _this.streamItems = [];
-        _this.queryInfo = new QueryInfo();
-        _this.spinner = new Spinner();
-        _this.streamService = new SearchStreamService();
-        _this.viewHolder = document.getElementsByClassName(viewHolderSelector)[0];
-        _this.domFunctions = new DOMFunctions();
-
-        _this.getStreamsList = function(streams, totalCount) {
-            _this.streamItems = streams;
-            _this.queryInfo.TotalCount = totalCount;
-            _this.renderTemplate(`<div class="totalStreams">Total results: {{TotalCount}}</div>
-                                    <div class="paging-control">
-                                        <span class="prev-page"></span>
-                                        <span>{{CurrentPage}}</span>
-                                        <span> / </span>
-                                        <span>{{TotalPages}}</span>
-                                        <span class="next-page"></span>
-                                    </div>
-                                    <div class="stream-list">
-                                        <ul class="streams"></ul>
-                                    </div>`);
-            _this.spinner.hide();
-        }
+    constructor() {
+        this.queryInfo = new QueryInfo();
+        this.spinner = new Spinner();
+        this.viewHolderSelector = "";
     }
 
     executeSearch(pageDirection) {
         this.queryInfo.CurrentPage += pageDirection;
         this.spinner.show();
-        this.streamService.getStreamCollection(this.queryInfo.SearchString, this.getStreamsList, this.queryInfo.CurrentPage);
+        const streamService = new SearchStreamService();        
+        streamService.getStreamCollection(this.queryInfo.SearchString, this.queryInfo.CurrentPage).then(result => {
+            this.queryInfo.TotalCount = result.totalCount;
+            this.renderTemplate(this.queryInfo, result.streamItems);
+            this.spinner.hide();
+        });
     }
 
-    activate(searchString) {
+    activate(searchString, viewHolderSelector) {
         this.queryInfo.SearchString = searchString;
+        this.viewHolderSelector = viewHolderSelector;
         this.executeSearch(0);
     }
 
-    renderTemplate(streamsListHtml){
-            streamsListHtml = this.domFunctions.getTemplateWithData(this.queryInfo, streamsListHtml);
-            this.viewHolder.innerHTML = streamsListHtml;
+    renderTemplate(queryInfo, streamItems) {
+        const domFunctions = new DOMFunctions();
+        const prevBtnVisibilityClass = queryInfo.CurrentPage > 1 ? "" : " invisible";
+        const nextBtnVisibilityClass = queryInfo.TotalPages > queryInfo.CurrentPage ? "" : " invisible";
+        const pagingVisibilityClass = queryInfo.TotalCount > 0 ? "" : " invisible";
+        const viewHolder = domFunctions.getElementByClassName(this.viewHolderSelector);
+        
+        viewHolder.innerHTML = `<div class="totalStreams">Total results: ${queryInfo.TotalCount}</div>
+                                <div class="paging-control${pagingVisibilityClass}">
+                                    <span class="prev-page${prevBtnVisibilityClass}"></span>
+                                    <span>${queryInfo.CurrentPage}</span>
+                                    <span> / </span>
+                                    <span>${queryInfo.TotalPages}</span>
+                                    <span class="next-page${nextBtnVisibilityClass}"></span>
+                                </div>
+                                <div class="stream-list">
+                                    <ul class="streams"></ul>
+                                </div>`;
 
-            if (this.queryInfo.TotalCount > 0) {
-                const nextPageButton = this.domFunctions.getElementByClassName("next-page");
-                if(this.queryInfo.TotalPages > this.queryInfo.CurrentPage) {
-                    nextPageButton.addEventListener("click", this.executeSearch.bind(this, 1));
-                }
-                else {
-                    nextPageButton.className += " invisible";
-                }
-
-                const prevPageButton = this.domFunctions.getElementByClassName("prev-page");
-                if(this.queryInfo.CurrentPage > 1) {
-                    prevPageButton.addEventListener("click", this.executeSearch.bind(this, -1));
-                }
-                else {
-                    prevPageButton.className += " invisible";
-                }
+        if (queryInfo.TotalCount > 0) {
+            if(queryInfo.TotalPages > queryInfo.CurrentPage) {
+                domFunctions.bindEventByClassName("click", "next-page", this.executeSearch.bind(this, 1))
             }
-            else {
-                const pagingControl =  this.domFunctions.getElementByClassName("paging-control");  
-                pagingControl.className += " invisible";
-            }
-            const streamItemsHolder = this.domFunctions.getElementByClassName("streams");
 
-            for (let i = 0; i < this.streamItems.length; i++) { 
-                streamItemsHolder.insertAdjacentHTML("beforeend", this.domFunctions.getTemplateWithData(this.streamItems[i], `<li>
-                                    <div class="stream-preview">
-                                        <span></span><img src="{{PreviewImageUrl}}" />
+            if(queryInfo.CurrentPage > 1) {
+                domFunctions.bindEventByClassName("click", "prev-page", this.executeSearch.bind(this, -1))
+            }
+        }
+
+        const streamItemsHolder = domFunctions.getElementByClassName("streams");
+        streamItems.forEach(streamItem => { 
+            streamItemsHolder.insertAdjacentHTML("beforeend", `<li>
+                                <div class="stream-preview">
+                                    <span></span><img src="${streamItem.PreviewImageUrl}" />
+                                </div>
+                                <div class="stream-details">
+                                    <h3>
+                                        <a href="${streamItem.StreamUrl}" class="stream-name">${streamItem.DisplayName}</a>
+                                    </h3>
+                                    <div class="game-name">
+                                        <span>${streamItem.GameName}</span> - <span>${streamItem.Viewers}</span> viewers
                                     </div>
-                                    <div class="stream-details">
-                                        <h3>
-                                            <a href="{{StreamUrl}}" class="stream-name">{{DisplayName}}</a>
-                                        </h3>
-                                        <div class="game-name">
-                                            <span>{{GameName}}</span> - <span>{{Viewers}}</span> viewers
-                                        </div>
-                                        <div class="game-description">{{Description}}</div>
-                                    </div>
-                                </li>`));
-            }
+                                    <div class="game-description">${streamItem.Description}</div>
+                                </div>
+                            </li>`);
+        });
     }
 }
 
 class SearchListController {
     constructor() {
         this.searchString = "";
-        this.domFunctions = new DOMFunctions();
     }
 
     init() {
-        this.renderTemplate(`<div class="search-list-view">
+        this.renderTemplate();
+    }
+
+    refreshSearchList() {
+        if (!this.searchString || this.searchString === "") {
+            alert("Search query is required. Please enter the value.");
+        } else {
+            const streamsList = new StreamsList();           
+            streamsList.activate(this.searchString, "search-list");
+        }
+    }
+
+    setSearchString(event) {
+        event.preventDefault();
+        if (event.keyCode == 13) {
+            this.refreshSearchList();
+        } else {
+            this.searchString = event.target.value;
+        }
+    }
+
+    renderTemplate() {
+        const output = document.getElementById("applicationHost");
+        output.insertAdjacentHTML("beforeend", `<div class="search-list-view">
                                     <div class="header-controls">
                                         <div class="search-control">
                                             <input type="text" class="searchString" placeholder="Search query..." />
@@ -247,33 +232,9 @@ class SearchListController {
                                     </div>
                                     <div class="search-list"></div>
                                 </div>`);
-    }
-
-    refreshSearchList() {
-        if (!this.searchString || this.searchString === "") {
-            alert("Search query is required. Please enter the value.");
-        }else {
-            const streamsList = new StreamsList("search-list");           
-            streamsList.activate(this.searchString);
-        }
-    }
-
-    setSearchString(event) {
-        event.preventDefault();
-        if (event.keyCode == 13) {
-            this.refreshSearchList();
-        }
-        else {
-            this.searchString = event.target.value;
-        }
-    }
-
-    renderTemplate(searchListViewHtml) {
-        const output = document.getElementById("applicationHost");
-        output.insertAdjacentHTML("beforeend", searchListViewHtml);
-
-        this.domFunctions.bindEventByClassName("click", "executeSearch", this.refreshSearchList.bind(this));
-        this.domFunctions.bindEventByClassName("keyup", "searchString", this.setSearchString.bind(this));
+        const domFunctions = new DOMFunctions();
+        domFunctions.bindEventByClassName("click", "executeSearch", this.refreshSearchList.bind(this));
+        domFunctions.bindEventByClassName("keyup", "searchString", this.setSearchString.bind(this));
     }
 
     static init() {
@@ -288,4 +249,6 @@ class App {
     };
 }
 
-App.init();
+{
+    App.init();
+}
